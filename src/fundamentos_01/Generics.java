e5783a6d209a1549c44b617ba1f8bdd6df71db91
RobertContrerasDev <rcontrerasmamani@gmail.com>
2026-07-09 05:56:25 -0500

    feat: primeros ejemplos de clases generics

--- src/fundamentos_01/Generics.java
+++ src/fundamentos_01/Generics.java
@@ -1,5 +1,21 @@
 package fundamentos_01;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Generics {
+
+    public static void main(String[] args) {
+
+        List lista = new ArrayList();
+
+        lista.add("texto");
+        lista.add(42); // esto compilaba, aunque mezclar tipos así es un error de diseño
+
+        String texto = (String) lista.get(0); // casting manual obligatorio
+        Integer numero = (Integer) lista.get(1);
+
+        String otro = (String) lista.get(1); // compila, pero explota en RUNTIME: ClassCastException
+    }
 
 }
