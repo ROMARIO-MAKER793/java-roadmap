1d590d8be08f51d4306067cb06a8e320625e3d9e
RobertContrerasDev <rcontrerasmamani@gmail.com>
2026-08-02 12:28:18 -0500

    feat:ejercicios con generics

---
+++ src/fundamentos_01/Caja.java
@@ -0,0 +1,31 @@
+package fundamentos_01;
+
+public class Caja<T> {
+
+    private T contenido;
+
+    public void guardar (T contenido){
+        this.contenido = contenido;
+    }
+
+    public T obtener () {
+        return contenido;
+    }
+
+    public static void main(String[] args) {
+
+        Caja<String> cajaTexto = new Caja<>();
+        cajaTexto.guardar("hola");
+        String valor = cajaTexto.obtener();
+
+        System.out.println(valor);
+
+        Caja<Integer> cajaNumero = new Caja<>();
+        cajaNumero.guardar(42);
+        Integer valorNumero = cajaNumero.obtener();
+
+        System.out.println(valorNumero);
+
+
+    }
+}
--- src/fundamentos_01/Generics.java
+++ src/fundamentos_01/Generics.java
@@ -7,15 +7,23 @@ public class Generics {
 
     public static void main(String[] args) {
 
-        List lista = new ArrayList();
+        /* List lista = new ArrayList();
 
         lista.add("texto");
         lista.add(42); // esto compilaba, aunque mezclar tipos así es un error de diseño
+
+        System.out.println(lista);
 
         String texto = (String) lista.get(0); // casting manual obligatorio
         Integer numero = (Integer) lista.get(1);
 
         String otro = (String) lista.get(1); // compila, pero explota en RUNTIME: ClassCastException
+        */
+
+        List<String> lista = new ArrayList<>();
+
+        lista.add("texto");
+        lista.add("");
     }
 
 }
