4ee886ecb4b78817c5e0db803c3fc054a826ecd6
RobertContrerasDev <rcontrerasmamani@gmail.com>
2026-08-03 23:29:34 -0500

    feat:Ejerciciso con Generics 01

---
+++ src/fundamentos_01/generics/Par.java
@@ -0,0 +1,26 @@
+package fundamentos_01.generics;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Par<T> {
+
+
+    private T numero1;
+
+    private T numero2;
+
+
+/*
+    public Par<T> (T numero1 , T numero2){
+        this.numero1 = numero1;
+        this.numero2 = numero2;
+    }
+
+
+*/
+
+
+}
