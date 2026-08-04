d5f71f3818ef80ba07c3aff08f9af995789266f9
RobertContrerasDev <rcontrerasmamani@gmail.com>
2026-08-04 06:03:41 -0500

    feat:Ejercicios 2 Generics

--- src/fundamentos_01/generics/Par.java
+++ src/fundamentos_01/generics/Par.java
@@ -7,20 +7,41 @@ import java.util.List;
 
 public class Par<T> {
 
-
     private T numero1;
 
     private T numero2;
 
-
-/*
-    public Par<T> (T numero1 , T numero2){
+    public void guardar (T numero1 , T numero2){
         this.numero1 = numero1;
         this.numero2 = numero2;
     }
 
+    public void obtener()
 
-*/
+    public void intercambiar (){
+
+        T temporal =  numero1;
+
+        numero1 = numero2;
+
+        numero2 = temporal;
+
+    }
+
+    public static void  main(String[] args){
+        Par<Integer> parNum = new Par<>();
+        parNum.guardar(1,2);
+        System.out.println(parNum);
+
+        parNum.intercambiar();
+
+        System.out.println(parNum);
+
+    }
+
+
+
+
 
 
 }
