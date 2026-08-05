897fb71a03403e644cb787caf849d9ab110dfc91
RobertContrerasDev <rcontrerasmamani@gmail.com>
2026-08-04 23:53:58 -0500

    feat:incorporado metodotoString

--- src/fundamentos_01/generics/Par.java
+++ src/fundamentos_01/generics/Par.java
@@ -1,14 +1,8 @@
 package fundamentos_01.generics;
 
-import org.w3c.dom.ls.LSOutput;
-
-import java.util.ArrayList;
-import java.util.List;
-
 public class Par<T> {
 
     private T numero1;
-
     private T numero2;
 
     public void guardar (T numero1 , T numero2){
@@ -16,25 +10,35 @@ public class Par<T> {
         this.numero2 = numero2;
     }
 
-    public void obtener()
+    public T getNumero1(){
+        return numero1;
+    }
+
+    public T getNumero2() {
+        return numero2;
+    }
 
     public void intercambiar (){
 
         T temporal =  numero1;
-
         numero1 = numero2;
-
         numero2 = temporal;
+
+    }
+
+    @Override
+    public String toString(){
+        return "Par [" + numero1 + ", " + numero2 + "]";
 
     }
 
     public static void  main(String[] args){
         Par<Integer> parNum = new Par<>();
+
         parNum.guardar(1,2);
         System.out.println(parNum);
 
         parNum.intercambiar();
-
         System.out.println(parNum);
 
     }
