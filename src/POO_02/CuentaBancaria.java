3a2eed8824188aa6ba25d2b75af2db652f9d49ea
RobertContrerasDev <rcontrerasmamani@gmail.com>
2026-08-09 23:34:30 -0500

    feat:POO Introduccion

---
+++ src/POO_02/CuentaBancaria.java
@@ -0,0 +1,28 @@
+package POO_02;
+
+public class CuentaBancaria {
+    /*
+    public double saldo; // público, cualquiera lo cambia sin control
+
+    public static void main(String[] args) {
+        CuentaBancaria cuenta = new CuentaBancaria();
+        cuenta.saldo = -5000; // nada lo impide, esto no debería ser posible nunca
+    }*/
+
+    private double saldo;
+
+    public void retirar(double monto){
+        if(saldo < monto){
+            throw new IllegalArgumentException("Saldo insuficiente");
+        }
+
+        saldo -= monto;
+
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+
+}
