ad91ee39d13dce0fbf888621f47c3e6444258cef
RobertContrerasDev <rcontrerasmamani@gmail.com>
2026-08-13 22:35:40 -0500

    feat:Ejerciico con Composicion

--- src/POO_02/EmpleadoFree.java
+++ src/POO_02/EmpleadoFree.java
@@ -2,17 +2,17 @@ package POO_02;
 
 import java.util.List;
 
-public class EmpleadoFree extends Empleado{
+public class EmpleadoFree implements FIchable{
 
-   public void procesarNomina(List<Empleado> empleados){
-       for(Empleado e : empleados){
-           e.marcarAsitencia("8:00");
+   public void procesarNomina(List<FIchable> empleados){
+       for(EmpleadoFree e : empleados){
+           e.marcarAsistencia("8:00");
        }
    }
 
     public static void main(String[] args) {
         EmpleadoFree lancer = new EmpleadoFree();
 
-        lancer.marcarAsitencia("Soy Free brother, no marca entrada");
+        lancer.marcarAsistencia("Soy Free brother, no marca entrada");
     }
 }
