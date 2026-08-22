cb83cb34c5bd95faad24d9c4047566aae7b81bb2
RobertContrerasDev <rcontrerasmamani@gmail.com>
2026-08-21 20:12:58 -0500

    feat:Excepciones

---
+++ src/Excepciones/PruebaExcepcion.java
@@ -0,0 +1,35 @@
+package Excepciones;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PruebaExcepcion {
+
+    public static void main(String[] args) {
+/*
+        List<String> lista = new ArrayList<>();
+
+        lista.add("robert");
+        lista.add("julio");
+        lista.add("ana");
+
+        try{
+            lista.get(5);
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Si entro " + e.getMessage() + e.getClass().getSimpleName());
+        }*/
+
+        /*try {
+            int[] numeros = {1, 2, 3};
+            System.out.println(numeros[5]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Indice fuera de rango");
+        } catch (Exception e) {
+            System.out.println("Error génerico");
+
+
+        }*/
+
+
+    }
+}
