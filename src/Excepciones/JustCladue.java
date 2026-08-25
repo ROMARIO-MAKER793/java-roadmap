65c59a008395cb1eb5d4ac39cca88bc95a352322
RobertContrerasDev <rcontrerasmamani@gmail.com>
2026-08-24 23:06:43 -0500

    feat:JustClaude

---
+++ src/Excepciones/JustCladue.java
@@ -0,0 +1,17 @@
+package Excepciones;
+
+public class JustCladue {
+
+    private final double saldoActual;
+    private final double montoSolicitado;
+
+
+    public JustCladue(double saldoActual, double montoSolicitado) {
+
+        this.saldoActual = saldoActual;
+        this.montoSolicitado = montoSolicitado;
+    }
+
+    public double getSaldoActual() { return saldoActual; }
+    public double getMontoSolicitado() { return montoSolicitado; }
+}
