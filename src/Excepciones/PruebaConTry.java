f37e1cf501fbee5a91d19ea5c3b610753fe1071f
RobertContrerasDev <rcontrerasmamani@gmail.com>
2026-08-27 21:41:46 -0500

    final Excepciones

---
+++ src/Excepciones/PruebaConTry.java
@@ -0,0 +1,22 @@
+package Excepciones;
+
+public class PruebaConTry implements AutoCloseable{
+
+    public void usar(){
+        System.out.println("Ejecutnado un metodo");
+
+        throw new RuntimeException("boom");
+    }
+
+    @Override
+    public void close()  {
+        System.out.println("Cerrandome automaticamente xd");
+    }
+
+
+    public static void main(String[] args) {
+        try(PruebaConTry prueba = new PruebaConTry()) {
+            prueba.usar();
+        }
+    }
+}
