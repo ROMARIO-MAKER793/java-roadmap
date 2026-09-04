c0eea69c86f8582ac5692a4b9cdfc6e08897da38
RobertContrerasDev <rcontrerasmamani@gmail.com>
2026-09-03 22:01:45 -0500

    feat: Streams y su definicion

--- src/Streams_05/StreamsOld.java
+++ src/Streams_05/StreamsOld.java
@@ -1,24 +1,41 @@
 package Streams_05;
 
+import com.sun.security.jgss.GSSUtil;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class StreamsOld {
 
     public static void main(String[] args) {
 
-        List<String> nombres = List.of("Ana", "Carloss", "Beatriz", "David", "Elena");
-        List<String> resultado = new ArrayList<>();
+       // List<String> nombres = List.of("Ana", "Carloss", "Beatriz", "David", "Elena");
+        //List<String> resultado = new ArrayList<>();
 
-        for (String nombre : nombres){
-            if (nombre.length() > 3){
+        /*for (String nombre : nombres){
+            if (nombre.length() > 4){
+
                 resultado.add(nombre.toUpperCase());
             }
         }
 
-        Collections.sort(resultado);
+        Collections.sort(resultado);*/
+
+        /*List<String> resultado = nombres.stream()
+                        .filter(nombre -> nombre.length() > 4)
+                                .map(String::toUpperCase)
+                                        .sorted()
+                                                .collect(Collectors.toList());
+
         System.out.println(resultado);
 
+*/
+        List<Integer> numeros = List.of(1,2,3,4,5);
+        int suma = numeros.stream()
+                .reduce(0,(acum , actual) -> acum + actual);
+
+        System.out.println(suma);
     }
 }
